41942331b9ec9453f389db0d945f081860839629
jiheunkim <ziheun54@gmail.com>
2021-03-25 21:22:44 +0900

    commit main class 3/25

--- src/jiheunkimproject/kuir.java
+++ src/jiheunkimproject/kuir.java
@@ -5,20 +5,24 @@ import java.io.IOException;
 public class kuir {
 	public static void main(String[] args) throws IOException, Throwable {
 		// TODO Auto-generated method stub
-//		String pathToXml = args[0];
+		if(args == null || args.length < 2) {
+			System.out.println("ERROR: 인자를 입력하세요.");
+			return;
+		}
 		
-		makeCollection mc = new makeCollection();
-		makeKeyword mk = new makeKeyword();
+		if("-c".equals(args[0])) {
+			makeCollection mc = new makeCollection();
+			mc.makeXML(args[1]);
+		}else if("-k".equals(args[0])) {
+			makeKeyword mk = new makeKeyword();
+			mk.kkma(args[1]);
+		}
 		
-		// doc 초기파일 작성
-		mc.makeXML("src/data/");
-//		// body에 내용 채우기
-//		mc.makeBody(doc, pathToXml);
-//		
-		// 형태소 작업
-		mk.kkma("src/collection.xml");
-		// index XML파일 생성
-//		mk.makeBody("index", doc);
+//		// doc 초기파일 작성
+//		mc.makeXML("src/data/");
+
+//		// 형태소 작업
+//		mk.kkma("src/collection.xml");
 	}
 
 }
