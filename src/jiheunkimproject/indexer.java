bfcf5ee863a8c3c6128917df3f4cba1a3f1b493c
jiheunkim <ziheun54@gmail.com>
2021-04-01 02:22:49 +0900

    week04_FINAL

--- src/jiheunkimproject/indexer.java
+++ src/jiheunkimproject/indexer.java
@@ -1,16 +1,9 @@
 package jiheunkimproject;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
