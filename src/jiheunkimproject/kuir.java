287bf1b03adbdde3d186587191e42561dfa8dbed
jiheunkim <ziheun54@gmail.com>
2021-04-21 03:21:42 +0900

    6-7주차 실습 1차

--- src/jiheunkimproject/kuir.java
+++ src/jiheunkimproject/kuir.java
@@ -10,16 +10,20 @@ public class kuir {
 			return;
 		}
 		
+		// Arguments값: -c src/data/
 		if("-c".equals(args[0])) {
 			makeCollection mc = new makeCollection();
 			mc.makeXML(args[1]);
 		}else if("-k".equals(args[0])) {
+			// Arguments값: -k src/collection.xml
 			makeKeyword mk = new makeKeyword();
 			mk.kkma(args[1]);
 		}else if("-i".equals(args[0])) {
+			// Arguments값: -i src/index.xml
 			indexer ix = new indexer();
 			ix.makepost(args[1]);
 		}else if("-s".equals(args[0])) {
+			// Arguments값 예시: -s src/index.post -q "라면에는 면, 분말 스프가 있다."
 			searcher sc = new searcher();
 			if("-q".equals(args[2])) {
 				sc.CalcSim(args[1], args[3]);
--- src/jiheunkimproject/searcher.java
+++ src/jiheunkimproject/searcher.java
@@ -16,8 +16,9 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
 public class searcher {	
-	@SuppressWarnings({ "unchecked" })
+	@SuppressWarnings("unchecked")
 	public void CalcSim(String postname, String query) throws Exception, Throwable {
+		// cosine similarity 구하기
 		KeywordExtractor ke = new KeywordExtractor();
 		KeywordList kl = ke.extractKeyword(query, true);
 		String[] savekkma = new String[kl.size()];
@@ -36,49 +37,61 @@ public class searcher {
         
         Element root = document.getDocumentElement();
         NodeList list = root.getElementsByTagName("doc");
+        
         // html 문서 개수
      	int N = list.getLength();
      	
 		String[] title = new String[N];
+		double[] Sim = new double[N];
 		double[] inner_product = new double[N];
+		double Wq = 0;
         
      	for(int i=0;i<N;i++) {
         	Element element = (Element) list.item(i);
         	title[i] = getChildren(element, "title");
-            System.out.print(title[i] + "\t");
+        	System.out.print(title[i] + "\t");
+        	Sim[i] = 0;
         }
      	System.out.println();
      	
      	for(int k=0;k<kl.size();k++) {
      		Keyword kwrd = kl.get(k);
 			savekkma[k] = kwrd.getString();
+			Wq += Math.pow(1, 2);
      	}
+     	Wq = Math.sqrt(Wq);
      	
      	for(int k=0;k<savekkma.length;k++) {
      		if(hashMap.containsKey(savekkma[k])) {
      			List<String> value = hashMap.get(savekkma[k]);
      			for(int i=0;i<value.size()/2;i++) {
      				inner_product[Integer.parseInt(String.valueOf(value.get(i*2)))] += Double.parseDouble(String.valueOf(value.get(i*2+1)));
+     				Sim[Integer.parseInt(String.valueOf(value.get(i*2)))] += Math.pow(Double.parseDouble(String.valueOf(value.get(i*2+1))), 2);
      			}
      		}
      	}
      	
-     	int CalcSimsize = inner_product.length;
-     	int[] titlerank = new int[CalcSimsize];
-     	for(int i=0;i<CalcSimsize;i++) {
+     	int[] titlerank = new int[N];
+     	for(int i=0;i<N;i++) {
      		inner_product[i] = Math.round(inner_product[i]*100)/100.0;
+     		if(inner_product[i]==0) {
+     			Sim[i] = 0;
+     		}else {
+     			Sim[i] = Math.round(inner_product[i]/(Wq*Math.sqrt(Sim[i]))*100)/100.0;
+     		}
      		titlerank[i] = i;
-     		System.out.print(inner_product[i] + "\t");
+    		System.out.print(Sim[i]+"\t");
      	}
-     	System.out.println();
      	
-     	for(int i=0;i<CalcSimsize;i++) {
-     		for(int j=i+1;j<CalcSimsize;j++) {
-     			if(inner_product[i]<inner_product[j]) {
+     	System.out.println();
+		System.out.println("\n***코사인 유사도 기반 상위 3위 문서 title 출력***");
+     	for(int i=0;i<Sim.length;i++) {
+     		for(int j=i+1;j<Sim.length;j++) {
+     			if(Sim[i]<Sim[j]) {
          			int temp = titlerank[i];
          			titlerank[i] = titlerank[j];
          			titlerank[j] = temp;
-         		}else if(inner_product[i]==inner_product[j]) {
+         		}else if(Sim[i]==Sim[j]) {
          			if(titlerank[i]>titlerank[j]) {
          				int temp = titlerank[i];
              			titlerank[i] = titlerank[j];
@@ -87,7 +100,6 @@ public class searcher {
          		}
      		}
      	}
-     	System.out.println("***상위 3위 문서 title 출력***");
      	for(int i=0;i<3;i++) {
      		System.out.println((i+1) + "위: " + title[titlerank[i]]);
      	}
